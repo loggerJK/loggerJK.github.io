93df7acd0f7ab2dd37e0f4f5439ee56d28b6f77a
jiwon7258 <jiwon7258@gmail.com>
2021-07-27 21:35:23 +0900

    20210727

---
+++ server_start.sh
@@ -0,0 +1,6 @@
+cd ~/github/blog/
+/mnt/c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe http://localhost:4000
+/mnt/c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe https://github.com/loggerJK/loggerJK.github.io
+/mnt/c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe https://imgur.com/
+bundle exec jekyll serve --livereload
+
