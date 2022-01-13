1888a87549539d2b72205d78e1d00ba5c97c37af
loggerJK <jiwon7258@gmail.com>
2022-01-13 20:27:35 +0900

    .

--- server_start.sh
+++ server_start.sh
@@ -1,6 +1,3 @@
-cd ~/github/blog/
-/mnt/c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe http://localhost:4000
-/mnt/c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe https://github.com/loggerJK/loggerJK.github.io
-/mnt/c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe https://imgur.com/
+cd ~/github/loggerJK.github.io
 bundle exec jekyll serve --livereload
 
